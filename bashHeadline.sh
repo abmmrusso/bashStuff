f0f025b0935d9928f4b47457967ddafc56db5c3a
abmmrusso <abmmrusso@users.noreply.github.com>
2017-11-21 17:31:12 +0000

    bashHeadline.sh now support breaking text into multiline headings

--- bashHeadline.sh
+++ bashHeadline.sh
@@ -1,9 +1,7 @@
 # @description Writes the given text within a '#' delimited headline
 #
-# The function will do its best to fill the whole of the terminal's
-# current horizontal space and try to centre the given text in the middle.
-# If using text that is wider than the terminal, headline formatting will 
-# break.
+# The function will do its best to centre the generated headline, with the
+# text being formatted to central alignment to the best possible effort.
 #
 # @example
 #   headline THIS IS A HEADLINE
@@ -12,33 +10,86 @@
 # @arg $@ Text to write in the headline
 #
 function headline() {
-    local TEXT="$(printf '%s' "$@")"
+
+    function writeHeadlineLine() {
+        local LINE=$1
+        local MAX_WRITABLE_WIDTH=$2
+
+        if [[ ${#LINE} -eq ${MAX_WRITABLE_WIDTH} ]]
+        then
+            printf "# %s #\n" "$LINE"
+        else
+            local BLANK_SPACING=$((MAX_WRITABLE_WIDTH - ${#LINE}))
+            local PREFIX_SPACING=$((BLANK_SPACING / 2))
+            local SUFFIX_SPACING=${PREFIX_SPACING}
+            if [[ "$((PREFIX_SPACING * 2))" -ne "${BLANK_SPACING}" ]]
+            then
+                SUFFIX_SPACING=$((SUFFIX_SPACING + 1))
+            fi
+
+            printf "#"
+            printf "%*.*s" 0 ${PREFIX_SPACING} "$PADDING"
+            printf "%s" "${LINE}"
+            printf "%*.*s" 0 ${SUFFIX_SPACING} "$PADDING"
+            printf "#"
+            printf "\n"
+        fi
+    }
+
+    local TEXT="$(printf '%s' "$*")"
     local COLS=$(tput cols)
     local PADDING=$(printf '%0.s ' $(seq 1 $COLS))
-
-    local BLANK_SPACING=$((COLS - 2 - ${#TEXT}))
-
-    local PREFIX_SPACING=$((BLANK_SPACING / 2))
-    local SUFFIX_SPACING=${PREFIX_SPACING}
-
-    if [[ "$((PREFIX_SPACING * 2))" -ne "${BLANK_SPACING}" ]]
-    then
-        SUFFIX_SPACING=$((SUFFIX_SPACING + 1))
-    fi
+    local MAX_WRITABLE_WIDTH=$((COLS - 2))
+    local MAX_TEXT_WIDTH=$((MAX_WRITABLE_WIDTH - 2))
 
     #Write starting sequence of #
     printf "%0.s#" $(seq 1 $COLS)
     printf "\n"
 
     #Write headline text section
-    printf "#"
-    printf "%*.*s" 0 ${PREFIX_SPACING} "$PADDING"
-    printf "%s" "${TEXT}"
-    printf "%*.*s" 0 ${SUFFIX_SPACING} "$PADDING"
-    printf "#"
-    printf "\n"
+    local CURRENT_PHRASE=""
+    WORD_SEPARATOR=""
+    for val in ${TEXT}
+    do
+        if [[ ${#val} -gt ${MAX_TEXT_WIDTH} ]]
+        then
+            if [[ ${#CURRENT_PHRASE} -gt 0 ]]
+            then
+                writeHeadlineLine "${CURRENT_PHRASE}" ${MAX_WRITABLE_WIDTH}
+                CURRENT_PHRASE=""
+                WORD_SEPARATOR=" "
+            fi
+
+            for sval in $(echo $val | sed -r "s/(.{${MAX_TEXT_WIDTH}})/\1 /g")
+            do
+                if [[ ${#sval} -eq ${MAX_TEXT_WIDTH} ]]
+                then
+                    writeHeadlineLine "${sval}" ${MAX_WRITABLE_WIDTH}
+                else
+                    CURRENT_PHRASE="${CURRENT_PHRASE}${WORD_SEPARATOR}${sval}"
+                    WORD_SEPARATOR=" "
+                fi
+            done
+        else
+            if [[ $((${#CURRENT_PHRASE} + ${#WORD_SEPARATOR} + ${#val})) -gt ${MAX_TEXT_WIDTH} ]]
+            then
+                writeHeadlineLine "${CURRENT_PHRASE}" ${MAX_WRITABLE_WIDTH}
+                CURRENT_PHRASE=""
+                WORD_SEPARATOR=" "
+            fi
+            CURRENT_PHRASE="${CURRENT_PHRASE}${WORD_SEPARATOR}${val}"
+            WORD_SEPARATOR=" "
+        fi
+    done
+
+    if [[ ${#TEXT} -eq 0 || ${#CURRENT_PHRASE} -gt 0 ]]
+    then
+        writeHeadlineLine "${CURRENT_PHRASE}" ${MAX_WRITABLE_WIDTH}
+    fi
 
     #Write ending sequence of #
     printf "%0.s#" $(seq 1 $COLS)
     printf "\n"
+
+    unset -f writeHeadlineLine
 }
--- tests/bashHeadline_test.sh
+++ tests/bashHeadline_test.sh
@@ -4,7 +4,6 @@
 
 oneTimeSetUp() {
 	original_width=$(tput cols)
-	stty cols 10
 }
 
 oneTimeTearDown() {
@@ -32,6 +31,20 @@ testHeadlineWithOddWidthTextOnEvenWidthTerminal() {
 	assertEquals "${expected_output%x}" "${headline_output%x}"
 }
 
+testHeadlineWithEvenWidthTextWithMultipleWordsOnEvenWidthTerminal() {
+	stty cols 10
+	local headline_output="$(headline yes no; echo x)"
+	local expected_output="$(printf '##########\n# yes no #\n##########\n' ; echo x)"
+	assertEquals "${expected_output%x}" "${headline_output%x}"
+}
+
+testHeadlineWithOddWidthTextWithMultipleWordsOnEvenWidthTerminal() {
+	stty cols 10
+	local headline_output="$(headline no no; echo x)"
+	local expected_output="$(printf '##########\n# no no  #\n##########\n' ; echo x)"
+	assertEquals "${expected_output%x}" "${headline_output%x}"
+}
+
 testHeadlineWithEvenWidthTextOnOddWidthTerminal() {
 	stty cols 9
 	local headline_output="$(headline even; echo x)"
@@ -43,5 +56,47 @@ testHeadlineWithOddWidthTextOnOddWidthTerminal() {
 	stty cols 9
 	local headline_output="$(headline odd; echo x)"
 	local expected_output="$(printf '#########\n#  odd  #\n#########\n' ; echo x)"
+	assertEquals "${expected_output%x}" "${headline_output%x}"
+}
+
+testHeadlineWithEvenWidthTextWithMultipleWordsOnOddWidthTerminal() {
+	stty cols 11
+	local headline_output="$(headline yes no; echo x)"
+	local expected_output="$(printf '###########\n# yes no  #\n###########\n' ; echo x)"
+	assertEquals "${expected_output%x}" "${headline_output%x}"
+}
+
+testHeadlineWithOddWidthTextWithMultipleWordsOnOddWidthTerminal() {
+	stty cols 11
+	local headline_output="$(headline no no; echo x)"
+	local expected_output="$(printf '###########\n#  no no  #\n###########\n' ; echo x)"
+	assertEquals "${expected_output%x}" "${headline_output%x}"
+}
+
+testHeadlineWithTextWiderThanTerminalExpectingFittedMultilineOutput() {
+	stty cols 10
+	local headline_output="$(headline '123456123456'; echo x)"
+	local expected_output="$(printf '##########\n# 123456 #\n# 123456 #\n##########\n' ; echo x)"
+	assertEquals "${expected_output%x}" "${headline_output%x}"
+}
+
+testHeadlineWithTextWiderThanTerminalExpectingMultilineOutput() {
+	stty cols 10
+	local headline_output="$(headline '1234561234'; echo x)"
+	local expected_output="$(printf '##########\n# 123456 #\n#  1234  #\n##########\n' ; echo x)"
+	assertEquals "${expected_output%x}" "${headline_output%x}"
+}
+
+testHeadlineWithTwoWordsWiderThanTerminalExpectingMultilineOutput() {
+	stty cols 10
+	local headline_output="$(headline '1234561234 1234561234'; echo x)"
+	local expected_output="$(printf '##########\n# 123456 #\n#  1234  #\n# 123456 #\n#  1234  #\n##########\n' ; echo x)"
+	assertEquals "${expected_output%x}" "${headline_output%x}"
+}
+
+testMultilineHeadlineTextWithPhrase() {
+	stty cols 10
+	local headline_output="$(headline 'This is a test phrase'; echo x)"
+	local expected_output="$(printf '##########\n#  This  #\n#  is a  #\n#  test  #\n# phrase #\n##########\n' ; echo x)"
 	assertEquals "${expected_output%x}" "${headline_output%x}"
 }
